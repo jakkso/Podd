4874e001d9764c494a902dac8a0d35cc3efd9870
Alexander Potts <alexander.potts@gmail.com>
2018-06-09 10:44:47 -0600

    Changed filehandler to a rotating file handler, changed .gitignore to account for new log file pattern

--- podd/utilities.py
+++ podd/utilities.py
@@ -2,6 +2,7 @@
 Contains logger utility function
 """
 import logging
+from logging.handlers import RotatingFileHandler
 from os import path
 
 
@@ -17,8 +18,8 @@ def logger(name, level=logging.DEBUG) -> logging.getLogger:
     fmt = logging.Formatter("%(asctime)s [%(filename)s] func: [%(funcName)s] [%(levelname)s] "
                             "line: [%(lineno)d] %(message)s")
     # delay=True delays opening file until actually needed, preventing I/O errors
-    file_hdlr = logging.FileHandler(f"{path.join(path.dirname(__file__), 'Logs/')}{name}.log",
-                                    delay=True)
+    file_hdlr = RotatingFileHandler(filename=f"{path.join(path.dirname(__file__), 'Logs/')}{name}.log",
+                                    delay=True, backupCount=5, maxBytes=2000000)
     file_hdlr.setLevel(level)
     file_hdlr.setFormatter(fmt)
     if not log.handlers:
