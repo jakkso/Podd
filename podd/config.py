61fd431453f3fa665cad286a8a96d713d4e34793
Alexander Potts <alexander.potts@gmail.com>
2018-06-10 21:31:42 -0600

    Added more logging calls & modified print statements

--- podd/config.py
+++ podd/config.py
@@ -4,10 +4,10 @@ from os import path
 class Config:
     """
     Edit the below entries to your desired preferences.
-
     As an example, I've used a setup that will work with Gmail, as far as host and port go.
+
+    By default, database file will be placed in the same directory as this file.
     Use absolute database locations to prevent pathing issues.
-    Database file will be placed in the same directory as this file.
     """
     host = 'smtp.gmail.com'
     port = 587
