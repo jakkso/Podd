9b76a0240616600a0ec84fd5d63098d5cb7764ea
Alexander Potts <alexander.potts@gmail.com>
2019-04-12 10:28:25 -0600

    iterate version

--- podd/settings.py
+++ podd/settings.py
@@ -18,5 +18,5 @@ class Config:
     host = "smtp.gmail.com"
     port = 587
     database = str(pathlib.Path(__file__).parent.joinpath("podcasts.db"))
-    version = "0.1.15"
+    version = "0.1.16"
     log_directory = None
